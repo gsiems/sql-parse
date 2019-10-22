80cbce956737dacc76d03780f971bce179b8729f
Gregory Siems <gsiems@gmail.com>
2019-10-21 19:55:37 -0500

    Add test case for checking number parsing

---
+++ sqlparse/testdata/input/t003.sql
@@ -0,0 +1,11 @@
+-- dialect: StandardSQL
+--- Numbers and Scientific notation
+SELECT -2.134 * +5E+6 AS col1,
+        78E-9 AS col2,
+        .123E4 AS col3,
+        ( 44.0 - 2 / 10 + 0.3 ) ** 1  AS col4,
+        -1 - 3 AS col5
+    WHERE 1 = 2
+        AND 4>3
+        AND +5E+6<=7
+ ;
