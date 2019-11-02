2b359a787c3aac5b1cf5fa71457c0059a71b379e
Gregory Siems <gsiems@gmail.com>
2019-11-01 21:57:06 -0500

    tweak due to uncertainty about validity of query

--- sqlparse/testdata/input/t003.sql
+++ sqlparse/testdata/input/t003.sql
@@ -1,9 +1,9 @@
--- dialect: StandardSQL
+-- dialect: PostgreSQL
 --- Numbers and Scientific notation
 SELECT -2.134 * +5E+6 AS col1,
         78E-9 AS col2,
         .123E4 AS col3,
-        ( 44.0 - 2 / 10 + 0.3 ) ** 1  AS col4,
+        ( 44.0 - 2 / 10 + 0.3 ) ^ 2  AS col4,
         -1 - 3 AS col5
     WHERE 1 = 2
         AND 4>3
